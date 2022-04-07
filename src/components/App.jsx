36aec3af0fe728a9cbcefee188f927d17153e011
Andresmagic <42359482+Andresmagic@users.noreply.github.com>
2022-04-07 08:33:58 -0500

    Testing the backend

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,31 @@
 import SideMenu from './SideMenu'
 import LandingPage from './LandingPage'
+import axios from "axios";
+import { useState } from 'react'
 
 function App() {
+  
+  const [profileData, setProfileData] = useState(null)
+
+
+  function getData() {    // Esto es para conseguir la data de flask 
+    axios({
+      method: "GET",
+      url:"/DMT",
+    })
+    .then((response) => {
+      const res =response.data
+      setProfileData(({
+        profile_name: res.name,
+        about_me: res.about}))
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+        }
+    })}
+
   return (
     <div className='bg-slate-100 h-max'>
       <div className='flex'>
@@ -9,8 +33,17 @@ function App() {
           <SideMenu />
         </div>
         <LandingPage />
+        <div>
+        <p>Prueba del backend, pulsale: </p><button onClick={getData}>Click me</button>
+        {profileData && <div>
+              <p>Profile name: {profileData.profile_name}</p>
+              <p>About me: {profileData.about_me}</p>
+            </div>
+        }
+        </div>
       </div>
     </div>
+    
   )
 }
 
