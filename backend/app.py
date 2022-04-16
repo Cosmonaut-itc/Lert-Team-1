597e099d29c51c3f2cd4711963b41e88d6ac12bf
Felix De Haro <felixddhs@outlook.com>
2022-04-16 13:51:52 -0600

    adapted app.py to our project, only left login and @me, changed models.py to create a connection to db using psycopg2. Deleted db.sqlite bc we already have a db hosted in aws. Added documentation to app.py modified code.

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,23 @@
-from flask import Flask, jsonify, request, jsonify, session 
+from flask import Flask, jsonify, request, jsonify, session
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS, cross_origin
 from flask_session import Session
-from models import db, User
-from config import AplicationConfig
+from models import User
 
 app = Flask(__name__)
-app.config.from_object(AplicationConfig)
-
 bcrypt = Bcrypt(app)
 cors = CORS(app, supports_credentials=True)
-server_session = Session(app) 
-db.init_app(app)
+server_session = Session(app)
 
-with app.app_context():
-    db.create_all()
-    
 
+## This route is used to authenticate the user once is inside the app.
 @app.route("/@me")
 def get_current_user():
     user_id = session.get("user_id")
 
     if not user_id:
         return jsonify({"error": "Unauthorized"}), 401
-    
+
     user = User.query.filter_by(id=user_id).first()
     return jsonify({
         "id": user.id,
@@ -31,50 +25,32 @@ def get_current_user():
     })
 
 
-
-
-@app.route("/register", methods=["POST"])
-def register_user():
-    email = request.json["email"]
-    password = request.json["password"]
-
-
-    user_exists = User.query.filter_by(email=email).first() is not None
-
-    if user_exists:
-        return jsonify({"error": "User already exists"}), 409
-
-    hashed_password = bcrypt.generate_password_hash(password)
-    new_user = User(email=email, password=hashed_password)
-    db.session.add(new_user)
-    db.session.commit()
-
-
-    return jsonify({
-        "id": new_user.id,
-        "email": new_user.email,
-    })
-
+# Login route
 @app.route("/login", methods=["POST"])
 def login_user():
-    email = request.json["email"]
-    password = request.json["password"]
+    user = User()  #Calls the class created in models.py in order to create a connection to de aws server
+    email = "admin@ibm.com"  #Test email
+    #password = request.json["password"] TODO: Create a register form in order to encrypt the passwords in the db
+    conn = user.get_db_connection()  #Creates the connection to the db through the class in model.py
+    cur = conn.cursor() #Creates a cursor in order to make the queries to the db
+    cur.execute('SELECT * FROM users WHERE email = %ds' % email) #Makes the query to the bd in order to get the email
+    user = cur.fetchone() #Gets a tuple from the query above, it must look like (id, email, password, rol)
 
-
-    user = User.query.filter_by(email=email).first() 
-
-    if user is None:
+    if user is None: #Auth if the user is present in the bd
         return jsonify({"error": "Unauthorized"}), 401
 
-    if not bcrypt.check_password_hash(user.password, password):
-        return jsonify({"error": "Unauthorized"}), 401
+    #TODO: Create a register form
 
-    session ["user_id"] = user.id
+    # if not bcrypt.check_password_hash(user.password, password):
+    # return jsonify({"error": "Unauthorized"}), 401
+
+    session["user_id"] = user[0] #Stores the user id in the cookies
 
     return jsonify({
         "id": user.id,
         "email": user.email,
     })
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- backend/models.py
+++ backend/models.py
@@ -1,15 +1,13 @@
-from uuid import uuid4
-from flask_sqlalchemy import SQLAlchemy
-from uuid import uuid4
+import psycopg2
+import os
 
-db = SQLAlchemy()
 
-def get_uuid():
-    return uuid4().hex
+class User:
+    ## get_db_connection is a function that creates a connection with the aws server.
+    def get_db_connection(self):
+        conn = psycopg2.connect(host=os.environ["HOST_NAME"],
+                                database='lert',
+                                user=os.environ["USERNAME_DB"],
+                                password=os.environ["PASSWORD_DB"])
 
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
-    email = db.Column(db.String(345), unique=True)
-    password = db.Column(db.Text, nullable=False)
-    
+        return conn
