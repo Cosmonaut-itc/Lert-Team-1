284fba0ee57f29b2e0e68d6e0d938dcbc2ade25f
Felix De Haro <felixddhs@outlook.com>
2022-04-16 13:47:56 -0600

    added variables to store email and password

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import { LockClosedIcon } from '@heroicons/react/solid'
 
 
 export default function Login() {
+  const [email, setEmail] = useState<String>("");
+  const [password, setPassword] = useState<String>("");
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
@@ -21,6 +24,8 @@ export default function Login() {
                 id="email-address"
                 name="email"
                 type="email"
+                value = {email}
+                onChange={(e)=> setEmail(e.target.value)}
                 autoComplete="email"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 bg-gray-300 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
@@ -34,6 +39,8 @@ export default function Login() {
                 id="password"
                 name="password"
                 type="password"
+                value = {password}
+                onChange={(e)=> setPassword(e.target.value)}
                 autoComplete="current-password"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 bg-gray-300 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
