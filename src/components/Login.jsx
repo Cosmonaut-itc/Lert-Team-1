5db94901ffa205ee364460459ccfcfa760d038c9
Felix De Haro <felixddhs@outlook.com>
2022-04-19 10:17:30 -0500

    added console log

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -59,6 +59,7 @@ export default function Login() {
             <button
               type="submit"
               className="group relative w-full flex justify-center py-2 px-10 rounded-2xl border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={console.log("You logged in succesfully")}
             >
               <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                 {/* <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" /> */}
