e5f9ce6750da26e5224dff5fc4f0bcc2859964f8
dmojarrot <a01638460@itesm.mx>
2022-04-21 09:21:55 -0600

    Added React Router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,18 @@
-// import SideMenu from './SideMenu'
-// import LandingPage from './LandingPage'
+import SideMenu from './SideMenu'
 // import ManagerCard from './ManagerCard'
+import LandingPage from './LandingPage'
 import Login from './Login'
 
 function App() {
   return (
-
     <div className='bg-slate-100 h-max'>
-    {/* //   <div className='flex'>
-    //     <div className='h-screen'>
-    //       <SideMenu />
-    //     </div>
-    //     <LandingPage />
-    //   </div> */}
-        <Login/>
+      <div className='flex'>
+        <div className='h-screen'>
+          <SideMenu />
+        </div>
+        <LandingPage />
       </div>
-    
+    </div>
   )
 }
 
