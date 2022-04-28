ea63ec954f9f287303f279fc2bcd3891dabbc4c7
Felix De Haro <felixddhs@outlook.com>
2022-04-27 19:22:43 -0500

    Reverted merge changes pt 3

--- backend/app.py
+++ backend/app.py
@@ -20,51 +20,12 @@ app.secret_key= "jdjskadkaskdalsd"
 ## This route is used to authenticate the user once is inside the app.
 @app.route("/@me")
 def get_current_user():
-    user_id = session.get("user_id")
-
-    if not user_id:
-        return jsonify({"error": "Unauthorized"}), 401
-    user = User.query.filter_by(id=user_id).first()
-    return jsonify({
-        "id": user.id,
-        "email": user.email,
-    })
+    return "Hello World"
 
 
 @app.route("/register", methods=["POST"])
 def register_user():
-    email = request.json["email"]
-    password = request.json["password"]
-
-
-    user_exists = User.query.filter_by(email=email).first() is not None
-
-    if user_exists:
-        return jsonify({"error": "User already exists"}), 409
-
-    hashed_password = bcrypt.generate_password_hash(password)
-    new_user = User(email=email, password=hashed_password)
-
-    return jsonify({
-        "id": new_user.id,
-        "email": new_user.email,
-    })
-
-@app.route("/login", methods=["POST"])
-def login_user():
-    email = request.json["email"]
-    password = request.json["password"]
-
-
-    user = User.query.filter_by(email=email).first() 
-
-    if user is None:
-        return jsonify({"error": "Unauthorized"}), 401
-
-    if not bcrypt.check_password_hash(user.password, password):
-        return jsonify({"error": "Unauthorized"}), 401
-
-    session ["user_id"] = user.id
+    return "Hello World"
 
 # Login route
 @app.route("/login", methods=["POST"])
