1651e5fe238d30e2996ae6c2a6a3dd8af4c37544
Andresmagic <42359482+Andresmagic@users.noreply.github.com>
2022-04-28 17:54:13 -0500

    Register function done and requirements.txt added for an optimized way to install libraries in env

--- backend/app.py
+++ backend/app.py
@@ -18,12 +18,37 @@ with app.app_context():
 ## This route is used to authenticate the user once is inside the app.
 @app.route("/@me")
 def get_current_user():
-    return "Hello World"
+    user_id = session.get("user_id")
 
+    if not user_id:
+        return jsonify({"error": "Unauthorized"}), 401
 
+    user = User.query.filter_by(id=user_id).first()
+    return jsonify({
+        "id": user.id,
+        "email": user.email,
+    })
+
+# Register route 
 @app.route("/register", methods=["POST"])
 def register_user():
-    return "Hello World"
+    email = request.json["email"]
+    password = request.json["password"]
+
+    user_exists = User.query.filter_by(email=email).first() is not None
+
+    if user_exists:
+        return jsonify({"error": "User already exists"}), 409
+
+    hashed_password = bcrypt.generate_password_hash(password)
+    new_user = User(email=email, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({
+        "id": new_user.id,
+        "email": new_user.email,
+    })
 
 # Login route
 @app.route("/login", methods=["POST"])
@@ -31,17 +56,21 @@ def login_user():
     email = request.json["email"]
     password = request.json["password"]
 
-    user = User.query.filter_by(email=email).first()
+    user = User.query.filter_by(email=email).first() 
 
     if user is None:
-        return jsonify({"error": "Unauthorized"})
+        return jsonify({"error": "Unauthorized"}), 401
 
-    session["user_id"] = user.id
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"error": "Unauthorized"}), 401
+
+    session ["user_id"] = user.id
 
     return jsonify({
         "id": user.id,
-        "email": user.email
+        "email": user.email,
     })
+
 
 if __name__ == "__main__":
     app.run(debug=True)
