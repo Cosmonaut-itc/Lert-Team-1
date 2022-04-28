449d2fda78d372b644832d9d9d9df115605ed721
Felix De Haro <felixddhs@outlook.com>
2022-04-27 19:17:46 -0500

    Reverted merge changes

--- backend/models.py
+++ backend/models.py
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-from uuid import uuid4
-from flask_sqlalchemy import SQLAlchemy
-from uuid import uuid4
-
-db = SQLAlchemy()
-
-def get_uuid():
-    return uuid4().hex
-
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
-    email = db.Column(db.String(345), unique=True)
-    password = db.Column(db.Text, nullable=False)
-    
-=======
 import psycopg2
 import os
 
@@ -23,9 +6,9 @@ class User:
     ## get_db_connection is a function that creates a connection with the aws server.
     def get_db_connection(self):
         conn = psycopg2.connect(host=os.environ["HOST_NAME"],
-                                database='lert',
+                                database='postgres',
                                 user=os.environ["USERNAME_DB"],
                                 password=os.environ["PASSWORD_DB"])
 
         return conn
->>>>>>> Development
+
