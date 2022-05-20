e6e941881cdab32fd67bb385c56e9fadc3792558
dmojarrot <a01638460@itesm.mx>
2022-05-20 09:58:26 -0600

    Minor fix

--- src/components/Manager/Home.jsx
+++ src/components/Manager/Home.jsx
@@ -95,7 +95,7 @@ export default function Home() {
 
   return (
     <div className='pt-4 pl-10 w-full'>
-      <div className='flex items-center justify-end mb-10 md:p-4 mr-6'>
+      <div className='flex items-center justify-end pb-10 md:p-4 mr-6'>
         <div className='text-xl font-semibold text-gray-600 invisible md:visible'>
           Status:{' '}
         </div>
