6c2bbee0c14f75a3e4f9915af32e7a39f2cdbb8c
dmojarrot <a01638460@itesm.mx>
2022-06-06 19:11:49 -0500

    Minor change

--- src/components/OPSManager/Home.jsx
+++ src/components/OPSManager/Home.jsx
@@ -68,7 +68,7 @@ export default function Home() {
     <div className='pt-16 pl-10 h-screen'>
       <div className='flex items-center gap-7 justify-between'>
         <div className='text-2xl font-semibold text-gray-600'>Managers</div>
-        <div className='px-52 lg:px-0'>
+        <div className='w-8/12 sm:w-5/12 md:w-4/12 lg:w-3/12'>
           <SearchBar />
         </div>
         <Button
@@ -88,8 +88,8 @@ export default function Home() {
           Collective Download
         </Button>
       </div>
-      <div className='flex pt-3'>
-        <div className='grid grid-cols-3 gap-3'>
+      <div className='flex justify-center md:justify-start pt-3'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3'>
           <button className='flex justify-center items-center'>
             <PlusCircleIcon className=' flex h-16 w-16 text-blue-400 hover:text-blue-500 active:text-blue-600 justify-center' />
           </button>
