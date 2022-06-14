d98f43e216b0eedf7a7f3f4dea633fd9726ac26b
Andresmagic <42359482+Andresmagic@users.noreply.github.com>
2022-06-14 15:27:24 -0500

    Frontend test of a button and backend test eliminated because thats tested with postman

---
+++ src/components/__test__/button.test.js
@@ -0,0 +1,9 @@
+import { Button } from '@mui/material';
+import React from 'react';
+import { ReactDOM } from 'react';
+import LandingPage from './Shared/LandingPage'
+
+it("it renders without crashing", () => {
+            const div = document.createElement("div");
+            ReactDOM.render( < Button > < /Button>, div)
+            })
